231f3ba69e2691ea00ba36cd015f509e00cf1e91
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-14 16:20:04 -0600

    Add TailwindCSS Styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/container-queries')
+  ],
+  darkMode: 'class'
+}
+
