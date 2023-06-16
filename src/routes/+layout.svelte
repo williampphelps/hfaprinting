523bf4b34cf23012e98820d52765bb7da5b85dc6
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-16 14:36:27 -0600

    Add Skeleton UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
+    import '../theme.postcss';
+    import '@skeletonlabs/skeleton/styles/skeleton.css';
     import "../app.css";
-  </script>
-  
-  <slot />
+
+
+    import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+    import { autoModeWatcher } from '@skeletonlabs/skeleton';
+</script>
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
+<AppShell>
+    <svelte:fragment slot="header">
+		<AppBar>
+            <svelte:fragment slot="lead">
+                <span class="flex flex-row gap-2 items-center"><a href="/" class="font-bold">Higher Fine Arts</a> <small>Printing</small></span>
+            </svelte:fragment>
+            <svelte:fragment slot="trail">
+                <LightSwitch />
+            </svelte:fragment>
+        </AppBar>
+	</svelte:fragment>
+    <div>
+        <slot />
+    </div>
+</AppShell>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    require('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/container-queries')
+    require('@tailwindcss/container-queries'),
+    ...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
   ],
   darkMode: 'class'
 }
