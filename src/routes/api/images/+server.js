8baf25ce9e65e861444f3329809fa176261d918d
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-23 11:55:40 -0600

    Use base64 gcloud creds

--- src/routes/api/images/+server.js
+++ src/routes/api/images/+server.js
@@ -1,23 +1,11 @@
 import { json, error } from '@sveltejs/kit';
 import { Storage } from '@google-cloud/storage';
 
-import { GCLOUD_PROJECT_ID, GCLOUD_PRIVATE_KEY_ID, GCLOUD_PRIVATE_KEY, GCLOUD_CLIENT_EMAIL, GCLOUD_CLIENT_ID, GCLOUD_CERT_URL} from '$env/static/private';
+import { GCLOUD_CREDENTIALS } from '$env/static/private';
 
 const storage = new Storage({
     projectId: 'higherfinearts',
-    credentials: {
-        "type": "service_account",
-        "project_id": GCLOUD_PROJECT_ID,
-        "private_key_id": GCLOUD_PRIVATE_KEY_ID,
-        "private_key": GCLOUD_PRIVATE_KEY,
-        "client_email": GCLOUD_CLIENT_EMAIL,
-        "client_id": GCLOUD_CLIENT_ID,
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": GCLOUD_CERT_URL,
-        "universe_domain": "googleapis.com"
-    }      
+    credentials: JSON.parse(Buffer.from(GCLOUD_CREDENTIALS, 'base64').toString())
 });
 export async function GET(event) {
     let session = await event.locals.getSession()
