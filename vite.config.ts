5067efe840a49abd89b09cee87eae615e0a3719a
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-23 11:21:38 -0600

    Remove Build Config Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			external: [
-				'@skeletonlabs/skeleton',
-				'@skeletonlabs/skeleton/styles/skeleton.css'
-			]
-		}
-	}
+	plugins: [sveltekit()]
 });
