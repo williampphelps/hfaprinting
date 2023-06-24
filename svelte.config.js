c725ab16b923fc2a85fc0bf3ded2ba0c6b3cd9cd
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-24 12:46:16 -0600

    Remove Threlte Preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import adapter from "svelte-kit-sst";
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import seqPreprocessor from 'svelte-sequential-preprocessor'
-import { preprocessThrelte } from '@threlte/preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: seqPreprocessor([vitePreprocess(), preprocessThrelte()]),
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
   },
