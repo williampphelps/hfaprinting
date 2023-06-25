654ee08be2edd211cd47a65c0ea7753cc11fd647
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-06-24 22:32:33 -0600

    Use Env Var for SHippo Key

--- src/routes/api/shipping/validate_address/+server.js
+++ src/routes/api/shipping/validate_address/+server.js
@@ -1,7 +1,8 @@
 import { json, error } from '@sveltejs/kit';
+import { SHIPPO_KEY } from '$env/static/private';
 import shippo from 'shippo';
 
-const Shippo = shippo('shippo_test_7929d84394d6372f13f7aa2e4ce88e52074db045')
+const Shippo = shippo(SHIPPO_KEY)
 export async function POST({ request }) {
     const address = await request.json();
     
