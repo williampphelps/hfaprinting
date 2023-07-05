92c3d231618f65618928926c0948d3e024ecc576
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-07-05 17:54:08 -0600

    Try to catch termination

--- src/lib/server/models/dbConnect.js
+++ src/lib/server/models/dbConnect.js
@@ -4,7 +4,14 @@ import { MONGODB_URI, DATABASE_NAME } from '$env/static/private';
 
 let connection = {};
 
-async function dbConnect() {
+async function dbConnect(action = 'connect') {
+
+  if (action == 'connect') {
+    console.log('Trying to connect...')
+  }
+  if (action == 'disconnect') {
+    console.log('Trying to disconnect...')
+  }
   if (connection.isConnected) {
     return;
   }
@@ -16,6 +23,10 @@ async function dbConnect() {
   });
 
   connection.isConnected = db.connections[0].readyState;
+}
+
+async function dbDisconnect() {
+  mongoose.disconnect();
 }
 
 export default dbConnect;
